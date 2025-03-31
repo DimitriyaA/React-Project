f3622285e602afaa29ba4f1ef9b3b4c03a63a483
Dimitriya Atchkova <164073778+DimitriyaA@users.noreply.github.com>
2025-03-31 21:54:43 +0300

    404 page added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,36 +14,46 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import Spellbook from "../components/Spellbook";
 import AuthGuard from "../components/guards/AuthGuard";
 import GuestGuard from "../components/guards/GuestGuard";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import NotFound from "../components/NotFound"; // Импортирай компонента за 404
+import "../App.css";
 
 const AppRouter = () => {
     return (
         <ErrorBoundary>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={<CategoryPage />} />
-                <Route path="/catalog/:category" element={<CategoryPage />} />
-                <Route path="/search" element={<SearchCatalog />} />
-                <Route path="/item/:id" element={<ItemDetails />} />
-                <Route path="/spellbook" element={<Spellbook />} />
-                <Route path="/map" element={<MagicMap />} />
+            <div className="app-container">
+                <Navbar />
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/catalog" element={<CategoryPage />} />
+                        <Route path="/catalog/:category" element={<CategoryPage />} />
+                        <Route path="/search" element={<SearchCatalog />} />
+                        <Route path="/item/:id" element={<ItemDetails />} />
+                        <Route path="/spellbook" element={<Spellbook />} />
+                        <Route path="/map" element={<MagicMap />} />
 
-                {/* 🔒 Само за регистрирани потребители */}
-                <Route element={<AuthGuard />}>
-                    <Route path="/add-item" element={<AddItem />} />
-                    <Route path="/edit-item/:id" element={<EditItem />} />
-                    <Route path="/map/add" element={<AddLocation />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                </Route>
+                        {/* 🔒 Само за регистрирани потребители */}
+                        <Route element={<AuthGuard />}>
+                            <Route path="/add-item" element={<AddItem />} />
+                            <Route path="/edit-item/:id" element={<EditItem />} />
+                            <Route path="/map/add" element={<AddLocation />} />
+                            <Route path="/profile" element={<ProfilePage />} />
+                        </Route>
 
-                {/* 🔓 Само за гости */}
-                <Route element={<GuestGuard />}>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Route>
+                        {/* 🔓 Само за гости */}
+                        <Route element={<GuestGuard />}>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                        </Route>
 
-                {/* Пренасочване при невалиден URL */}
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+                        {/* Пренасочване при невалиден URL - 404 страница */}
+                        <Route path="*" element={<NotFound />} /> {/* Тук показваме компонента NotFound */}
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
         </ErrorBoundary>
     );
 };
